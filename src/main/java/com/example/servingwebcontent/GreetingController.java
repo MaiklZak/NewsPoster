e7e771db438c1c1a1bfc1abedcc476c2bae5ed78
maiklzak <maiklzak@gmail.com>
2021-03-10 22:14:03 +0300

    #2 JPA-Hibernate, Postgres, Message-Entity, Filter

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,12 +1,22 @@
 package com.example.servingwebcontent;
 
+import com.example.servingwebcontent.domain.Message;
+import com.example.servingwebcontent.repository.MessageRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+
+    private final MessageRepository messageRepository;
+
+    public GreetingController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -16,7 +26,29 @@ public class GreetingController {
 
     @GetMapping
     public String main(Model model) {
-        model.addAttribute("some", "hello, letsCode!");
+        Iterable<Message> messages = messageRepository.findAll();
+        model.addAttribute("messages", messages);
+        model.addAttribute("message", new Message());
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@ModelAttribute("message") Message message, Model model) {
+        messageRepository.save(message);
+        Iterable<Message> messages = messageRepository.findAll();
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(@ModelAttribute("message") Message message, Model model) {
+        Iterable<Message> messages;
+        if (message.getTag() != null && !message.getTag().isEmpty()) {
+            messages = messageRepository.findByTag(message.getTag());
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages", messages);
         return "main";
     }
 }
