608cdc3da5280b2531f9e2e36d6794b2da6b27b6
maiklzak <maiklzak@gmail.com>
2021-03-18 21:07:57 +0300

    #9 Mail, activated account, add MailConfig, MailSender, properties for outgoing messages

---
+++ src/main/java/com/example/servingwebcontent/config/MailConfig.java
@@ -0,0 +1,49 @@
+package com.example.servingwebcontent.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    String password;
+
+    @Value("${spring.mail.port}")
+    int port;
+
+    @Value("${spring.mail.protocol}")
+    String protocol;
+
+    @Value("${mail.debug}")
+    String debug;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+
+        properties.setProperty("mail.transport.protocol", protocol);
+        properties.setProperty("mail.debug", debug);
+//        properties.setProperty("mail.smtp.ssl.enable", "true");
+
+        return mailSender;
+    }
+}
--- src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
+++ src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
@@ -1,21 +1,21 @@
 package com.example.servingwebcontent.controller;
 
-import com.example.servingwebcontent.domain.Role;
 import com.example.servingwebcontent.domain.User;
-import com.example.servingwebcontent.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.servingwebcontent.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
 
 @Controller
 public class RegistrationController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/registration")
     public String registration() {
@@ -24,14 +24,22 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Model model) {
-        User userFromDB = userRepository.findByUsername(user.getUsername());
-        if (userFromDB != null) {
+        if (!userService.addUser(user)) {
             model.addAttribute("message", "User exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(@PathVariable String code, Model model) {
+        boolean isActivated = userService.activateUser(code);
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
     }
 }
