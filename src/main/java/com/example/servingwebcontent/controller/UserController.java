41b2becd950b647035f4fa8d302838f8b6770eab
maiklzak <maiklzak@gmail.com>
2021-03-19 20:24:44 +0300

    #10 add flyway, profile, migration DB, move logic from UserController to UserService

--- src/main/java/com/example/servingwebcontent/controller/UserController.java
+++ src/main/java/com/example/servingwebcontent/controller/UserController.java
@@ -2,36 +2,35 @@ package com.example.servingwebcontent.controller;
 
 import com.example.servingwebcontent.domain.Role;
 import com.example.servingwebcontent.domain.User;
-import com.example.servingwebcontent.repository.UserRepository;
+import com.example.servingwebcontent.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        List<User> userList = userRepository.findAll();
+        List<User> userList = userService.findAll();
         model.addAttribute("users", userList);
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{id}")
     public String userEditForm(@PathVariable("id") User user, Model model) {
 //        model.addAttribute("user", userRepository.getOne(id));
@@ -40,23 +39,26 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(@ModelAttribute User user,
                            @RequestParam Map<String, String> form) {
-
-        user.setRoles(userRepository.getOne(user.getId()).getRoles());
-
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-        userRepository.save(user);
+        userService.saveUser(user, form);
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(@AuthenticationPrincipal User user,
+                                @RequestParam String password,
+                                @RequestParam String email) {
+        userService.updateProfile(user, password, email);
+        return "redirect:/user/profile";
     }
 }
