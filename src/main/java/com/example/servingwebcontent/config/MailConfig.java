923408e16496560aeabe11cec6d4a0c044e6c5a7
maiklzak <maiklzak@gmail.com>
2021-03-21 22:24:49 +0300

    add PropertySource in MailConfig, move auth data from application.properties to emailSender.properties

--- src/main/java/com/example/servingwebcontent/config/MailConfig.java
+++ src/main/java/com/example/servingwebcontent/config/MailConfig.java
@@ -3,11 +3,13 @@ package com.example.servingwebcontent.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+@PropertySource("classpath:emailSender.properties")
 @Configuration
 public class MailConfig {
 
