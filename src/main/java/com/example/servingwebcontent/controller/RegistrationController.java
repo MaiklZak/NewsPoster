cd0b9217ed30b06b122546ffb5efefce4f41e53e
maiklzak <maiklzak@gmail.com>
2021-03-21 22:06:14 +0300

    #11/1 validation registration

--- src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
+++ src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -30,7 +31,8 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
         if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
-            model.addAttribute("passwordError", "Passwords are different!");
+            model.addAttribute("passwordErrorDif", "Passwords are different!");
+            return "registration";
         }
         if (bindingResult.hasErrors()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
--- src/main/java/com/example/servingwebcontent/service/UserService.java
+++ src/main/java/com/example/servingwebcontent/service/UserService.java
@@ -29,7 +29,13 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username);
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+
     }
 
     public boolean addUser(User user) {
@@ -68,6 +74,7 @@ public class UserService implements UserDetailsService {
             return false;
         }
         user.setActivationCode(null);
+        user.setPassword2("cannot be empty");
         userRepository.save(user);
         return true;
     }
